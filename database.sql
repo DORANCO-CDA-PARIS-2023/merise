ab89a0852f829dce0b1ba998417eaed45d529014
Philisavailable <bertola.philippe@gmail.com>
2024-02-19 16:39:02 +0100

    bdd

--- database.sql
+++ database.sql
@@ -1,35 +1,35 @@
 CREATE TABLE auteur (
-    ID_auteur INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(100),
     prenom VARCHAR(100)
 );
 
 CREATE TABLE genre (
-    ID_genre INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(100)
 );
 
 CREATE TABLE livre (
-    ID_livre INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre VARCHAR(255),
-    ID_auteur INT,
-    ID_genre INT,
-    FOREIGN KEY (ID_auteur) REFERENCES auteur(ID_auteur),
-    FOREIGN KEY (ID_genre) REFERENCES genre(ID_genre)
+    id_auteur INT,
+    id_genre INT,
+    FOREIGN KEY (ID_auteur) REFERENCES auteur(id),
+    FOREIGN KEY (ID_genre) REFERENCES genre(id)
 );
 
 CREATE TABLE etudiant (
-    ID_etudiant INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(100),
     prenom VARCHAR(100)
 );
 
 CREATE TABLE emprunt (
-    ID_emprunt INT PRIMARY KEY,
-    ID_livre INT,
-    ID_etudiant INT,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_livre INT,
+    id_etudiant INT,
     date_emprunt DATE,
     date_retour DATE,
-    FOREIGN KEY (ID_livre) REFERENCES livre(ID_livre),
-    FOREIGN KEY (ID_etudiant) REFERENCES etudiant(ID_etudiant)
+    FOREIGN KEY (id_livre) REFERENCES livre(id),
+    FOREIGN KEY (id_etudiant) REFERENCES etudiant(id)
 );
