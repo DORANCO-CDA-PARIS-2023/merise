cfabce07bf8c2a3f6b813cba11387dea59c8cfd1
Ludovic Anselin <lanselin@atakama-technologies.com>
2024-02-19 16:37:49 +0100

    [ADD] database.sql file to create tables

---
+++ database.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS auteur (
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS livre (
+    id INT NOT NULL AUTO_INCREMENT,
+    titre VARCHAR(100) NOT NULL,
+    date_de_parution DATETIME,
+    auteur_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(auteur_id) REFERENCES auteur(id)
+);
+
+CREATE TABLE IF NOT EXISTS etudiant (
+    id INT NOT NULL AUTO_INCREMENTS,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS emprunts (
+    id INT NOT NULL AUTO_INCREMENTS,
+    livre_id INT NOT NULL,
+    auteur_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(livre_id) REFERENCES livre(id),
+    FOREIGN KEY(auteur_id) REFERENCES auteur(id)
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+    id INT NOT NULL AUTO_INCREMENTS,
+    nom VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT NOT NULL AUTO_INCREMENTS,
+    livre_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(livre_id) REFERENCES livre(id),
+    FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
