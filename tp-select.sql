15882dddcadd1c68c299af4ecf889de634757fa9
Ludovic Anselin <lanselin@atakama-technologies.com>
2024-02-20 11:06:56 +0100

    [ADD] tp-select.sql (7 to 13)

--- tp-select.sql
+++ tp-select.sql
@@ -22,26 +22,38 @@ FROM book b JOIN author a ON b.id_author=a.id
 GROUP BY a.id;
 
 -- 7/ Affichez les livres qui ont été empruntés au moins une fois.
-
+SELECT book.* FROM borrow JOIN book ON borrow.id_book=book.id;
 
 
 -- 8/ Affichez les étudiants qui n'ont pas encore emprunté de livre.
-
+SELECT * FROM student s WHERE s.id NOT IN (SELECT id_student FROM borrow);
 
 -- 9/ Afficher la moyenne des années de naissance des auteurs.
-
+SELECT AVG(YEAR(birthday)) FROM author;
 
 -- 10/ Affichez le nombre d'emprunts effectués par chaque étudiant.
-
+SELECT s.name, s.firstname, COUNT(b.id) AS "Nombre d'emprunt" FROM student s JOIN borrow b ON s.id=b.id_student GROUP BY b.id;
 
 -- 11/ Affichez les détails des emprunts, y compris le titre du livre, le nom de l'auteur et le nom de l'étudiant.
-
+SELECT borrow.*, book.title, CONCAT(a.name, " ", a.firstname) AS "Auteur", CONCAT(s.name, " ", s.firstname) AS "Étudiant"
+FROM borrow 
+JOIN book ON borrow.id_book=book.id
+JOIN author a ON book.id_author=a.id 
+JOIN student s ON borrow.id_student=s.id;
 
 -- 12/ Affichez les livres qui ont été publiés au cours des trois dernières années.
-
+SELECT * FROM book WHERE year_publish >= YEAR(NOW()) - 3;
 
 -- 13/ Affichez les étudiants ayant emprunté plus de trois livres.
-
+SELECT * 
+FROM student s 
+WHERE s.id IN (
+    SELECT s.id 
+    FROM student s 
+    JOIN borrow b ON s.id=b.id_student 
+    GROUP BY b.id 
+    HAVING COUNT(*) >= 3
+);
 
 -- 14/ Affichez les livres, les auteurs et les emprunts associés pour un étudiant spécifique.
 
